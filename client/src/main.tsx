4909ab618b81ac7091e86660d29ed0a774c6cb2c
Lorenzo Patto <lorenzoqdpatto@gmail.com>
2023-12-06 22:35:10 -0300

    fix bugs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { globalStyles } from "./styles/globals";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
 import { Router } from "./route/router.tsx";
 
 globalStyles();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <Router />
   </React.StrictMode>
 );
